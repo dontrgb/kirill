9878d57e6bcab393a3660068a6e916f517a4cef0
kirill <zxcg2036@gmail.com>
2025-04-30 14:46:48 +0300

    margins

--- task.rb
+++ task.rb
@@ -12,57 +12,57 @@
 
 =end
 class Order
-def get_order
-  orders = []
-  i = 1
+  def get_order
+    orders = []
+    i = 1
 
-  puts "введите кол-во заказов"
-  count_orders = gets.to_i
+    puts "введите кол-во заказов"
+    count_orders = gets.to_i
 
-  count_orders.times do 
-    puts "введите название блюда № #{i}"
-    name_dish = gets.to_s.strip
-    puts "введите цену блюда"
-    cost_dish = gets.to_i
-    puts "введите кол-во блюд"
-    count_dish = gets.to_i
-    i+=1 
-    orders << {name: name_dish, cost: cost_dish, count: count_dish}
-  end
-
-  orders
-end
-
-def all_price
-  price = 0
-
-  get_order.each do |order|
-    if order[:count] > 3
-      price += (order[:cost] * order[:count]) * 0.85
-    else
-      price += (order[:cost] * order[:count])
+    count_orders.times do 
+      puts "введите название блюда № #{i}"
+      name_dish = gets.to_s.strip
+      puts "введите цену блюда"
+      cost_dish = gets.to_i
+      puts "введите кол-во блюд"
+      count_dish = gets.to_i
+      i+=1 
+      orders << {name: name_dish, cost: cost_dish, count: count_dish}
     end
+
+    orders
   end
 
-  price
-end
+  def all_price
+    price = 0
 
-def costly_dish
-  order = get_order
-  cost = 0
-  name = ''
-  order.each do |x|
-    if x[:cost] > cost
-      cost = x[:cost]
-      name = x[:name]
+    get_order.each do |order|
+      if order[:count] > 3
+        price += (order[:cost] * order[:count]) * 0.85
+      else
+        price += (order[:cost] * order[:count])
+      end
     end
-  end
-  "самое дорогое блюдо в заказе: #{name}, цена блюда: #{cost}" 
-end  
 
-def limit
-  if all_price > 10000
-    "сумма заказа превышает лимит"
+    price
   end
-end
+
+  def costly_dish
+   order = get_order
+    cost = 0
+    name = ''
+    order.each do |x|
+      if x[:cost] > cost
+        cost = x[:cost]
+        name = x[:name]
+      end
+    end
+    "самое дорогое блюдо в заказе: #{name}, цена блюда: #{cost}" 
+  end  
+
+  def limit
+    if all_price > 10000
+      "сумма заказа превышает лимит"
+  end
+  end
 end
